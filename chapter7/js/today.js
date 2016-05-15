5d85c76d34faebf902ec807e485b082f9888e2f0
carmenlm <carmenlopezmedina@yahoo.es>
2016-05-15 22:35:22 +0200

    upload

--- chapter7/js/today.js
+++ chapter7/js/today.js
@@ -1,37 +1,43 @@
-// today.js #2
-// This script indicates the current date and time.
+/**
+ * Created by Carmen on 12/05/2016.
+ */
 
-// This function is used to update the text of an HTML element.
-// The function takes two arguments: the element's ID and the text message.
+/**
+ * funcion que va a poner un texto con la fecha de hoy en el nodo que pasemos
+ * @param elementId -- nodo donde se va a poiner el mensaje
+ * @param message -- mensaje que queremos poner
+ */
 function setText(elementId, message) {
     'use strict';
-    
-    if ( (typeof elementId == 'string')
-    && (typeof message == 'string') ) {
-    
-        // Get a reference to the paragraph:
+
+    if ((typeof elementId == 'string')
+        && (typeof message == 'string')) {
+
+        //capturo el nodo pasondo al document el elementid
         var output = document.getElementById(elementId);
-    
-        // Update the innerText or textContent property of the paragraph:
-		if (output.textContent !== undefined) {
-			output.textContent = message;
-		} else {
-			output.innerText = message;
-		}
-    
-    } // End of main IF.
 
-} // End of setText() function.
+        //actualizo el innertext con el mensaje
+        if (output.textContent !== undefined) {
+            output.textContent = message;
+        } else {
+            output.innerText = message;
+        }
 
-// Call this function when the page has loaded:
-function init() {
+    }
+
+}
+
+function initialize() {
     'use strict';
+    //capturo la fecha de hoy  // objeto date
     var today = new Date();
-    var message = 'Right now it is ' + today.toLocaleDateString();
-    message += ' at ' + today.getHours() + ':' + today.getMinutes();
+    //mensaje que quiero poner en el nodo
+    var message = 'Hoy es ' + today.toLocaleDateString();
+    message += ' y son las ' + today.getHours() + ':' + today.getMinutes();
 
-    // Update the page:
+    // llamo a la funcion para poner el mensaje
     setText('output', message);
-    
-} // End of init() function.
-window.onload = init;
+
+}
+
+window.onload = initialize;
